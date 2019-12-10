f83f685ff732de35f8b6f7bf9a3e10dacbec56fd
Lukasz Tolpa <s15525@pjwstk.edu.pl>
2019-12-10 18:59:29 +0100

    repair paneladministratora

--- controller/wyborcaController.js
+++ controller/wyborcaController.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const wyborca = require('../model/Wyborca');
 
 router.get("/", (req, res, next) => {
-    const userList = User.list();
-    res.render('users/userList', {userList: userList});
+    const wyborcaList = wyborca.list();
+    res.render('/PanelAdministratora', {wyborcaList: wyborcaList});
 });
 
 router.get("/newRekord", (req, res, next) => {
     res.render('users/userForm', { pageTitle: "Nowy użytkownik", formAction: "add", user: {} });
 });
+
+module.exports.route = router;
